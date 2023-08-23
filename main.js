c5f51fd5fe0c8bef2003a1401e3aa60c446db621
Aurelie Gedeon <agedeon540@gmail.com>
2023-08-23 18:39:32 -0500

    set up scene and camera

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 import './style.css'
 
+import * as THREE from 'three'
 
+const sizes = {
+  height: window.innerHeight,
+  width: window.innerWidth
+}
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+})
 
