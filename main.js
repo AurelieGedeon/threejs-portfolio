66f4974947b012820b4aab6f2cb39fc00ea3f671
Aurelie Gedeon <agedeon540@gmail.com>
2023-08-23 19:22:47 -0500

    added light; added more labelling

--- main.js
+++ main.js
@@ -7,10 +7,17 @@ const sizes = {
   width: window.innerWidth
 }
 
+//Scene
 const scene = new THREE.Scene()
 
+
+
+//Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
 
+
+
+//Render
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg')
 })
@@ -19,12 +26,27 @@ renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(sizes.width, sizes.height)
 camera.position.setZ(30)
 
-renderer.render(scene, camera)
 
+
+//Geometry
 const geometry = new THREE.TorusGeometry(10, 1, 16, 100)
-const material = new THREE.MeshBasicMaterial({
-  color: 0xFF6347,
-  wireframe: true
-});
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
+
 const torus = new THREE.Mesh( geometry, material )
 scene.add(torus)
+
+
+//Light
+const pointLight = new THREE.PointLight(0xffffff)
+
+
+function animate() {
+  requestAnimationFrame( animate )
+
+  torus.rotateX(0.01)
+  torus.rotateY(0.005)
+  torus.rotateZ(0.01)
+  renderer.render( scene, camera )
+}
+
+animate()
